66f0433bf88ac84bf2076b760225721010062b1d
Er2105 <erik.dittmann@studmail.w-hs.de>
2026-07-02 17:44:04 +0200

    Kommentiert, mögliche nächste Arbeitsschritte

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import argparse, time
 import torch
-from torch.utils.data import DataLoader
-import optuna
+from torch.utils.data import DataLoader #Wandelt Dataset in Batches um
+import optuna #Bibliothek für Hyperparameter-Optimierung
 
 from src.utils import setup_env
 from src.data import YawDDDataset
-from src.training import trainer, YawDDclassifier
+from src.training import trainer, YawDDclassifier #importiert Modell und Trainingsfunktion
 from src.evaluation import evaluate
 
 
@@ -15,13 +15,20 @@ from src.evaluation import evaluate
 # * Tensorboard
 # * Logging of results / save (best) model
 
+
+# Trial: Versuch mit bestimmten Hyperparametern (Optuna), wird mehrfach aufgerufen
 def objective(trial):
 
     # training hyperparameters to tune
+    # Batch Size, Optuna wählt entweder 4 oder 8
     args.batch_size = trial.suggest_categorical("batch_size", [4, 8])
+    # Friert zufällig ein 0 = trainieren, 1 = einfrieren
     args.freeze_backbone = trial.suggest_categorical("freeze_backbone", [0, 1])
+    # Lernrate zwischen 0.00001 und 0.001, logarithmisch verteilt.
     args.lr = trial.suggest_float("lr", 1e-5, 1e-3, log=True)
+    # Dropout zwischen 0.2 - 0.6, in 0.1 Schritten 
     args.dropout = trial.suggest_float("dropout", 0.2, 0.6, step=0.1)
+    # Gibt aktuelle Hyperparameter aus
     print(f'=================================================================')
     print(f' batch_size: {args.batch_size}, freeze_backbone: {args.freeze_backbone}, lr: {args.lr:0.5f}, dropout: {args.dropout:0.1f}')
 
@@ -29,19 +36,25 @@ def objective(trial):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # data preparation
+    # Drei Splits, Wie viele Frames pro Video
     trainset = YawDDDataset('train', num_frames=args.num_frames)
     valset = YawDDDataset('val', num_frames=args.num_frames)
     testset = YawDDDataset('test', num_frames=args.num_frames)
 
     # dataloaders
+    # Shuffle mischt Daten durch, drop_last entfernt unvollständige Batches, num_workers = 0 Daten werden im Hauptprozess geladen,
+    # keine Parallelisierung. Sonst würde neben dem Training schon der nächste Batch vorbereitet werden, um GPU auszulasten
+    # Könnte hier etwa auf CPU-Kerne/2 hochgesetzt werden AUSPROBIEREN
     trainloader = DataLoader(trainset, batch_size=args.batch_size, num_workers=0, shuffle=True, drop_last=True)
     valloader = DataLoader(valset, batch_size=args.batch_size, num_workers=0, shuffle=False)
     testloader = DataLoader(testset, batch_size=args.batch_size, num_workers=0, shuffle=False)
 
     # model
+    # Initialisiert Modell, schieb es auf Device
     model = YawDDclassifier(args.dropout).to(device)
     
     # start training
+    # Übergibt Daten, Modell, Hyperparameter, gibt beste F1 und die entsprechende Epoche zurück
     f1_val, epoch = trainer(trainloader=trainloader,
             valloader=valloader,
             model=model,
@@ -52,14 +65,15 @@ def objective(trial):
             )
     
     # Decide if trial should be pruned
-    trial.report(f1_val, epoch)
-    if trial.should_prune():
+    trial.report(f1_val, epoch) # Meldet Zwischenergebnis an Optuna
+    if trial.should_prune(): # schlechte Trials werden früh abgebrochen, um Zeit zu sparen
         raise optuna.TrialPruned()
     
     # test
+    # Bewertet Modell auf Testdaten
     test_metrics = evaluate(testloader, model, device)
     print(f"=================================================================\nTest Acc: {test_metrics['accuracy']:.3f}") 
-    return f1_val
+    return f1_val #Optuna optimiert diesen Wert
 
 
 if __name__ == "__main__":
@@ -67,21 +81,24 @@ if __name__ == "__main__":
     start_timestamp = time.time()
 
     # set seed and precision
+    # setzt Seed für reproduzierbare Ergebnisse
     setup_env(seed=0)    
 
     # get args 
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, default='YawDD')
-    parser.add_argument("--num_frames", type=int, default=64) #64->2
-    parser.add_argument("--epochs", type=int, default=3) #15 ->2
-    parser.add_argument("--n_trials", type=int, default=2) #40->2
-    args = parser.parse_args()
+    parser.add_argument("--num_frames", type=int, default=64) # Anzahl Frames pro Sample
+    parser.add_argument("--epochs", type=int, default=2) # Anzahl Trainingsdurchläufe
+    parser.add_argument("--n_trials", type=int, default=10) # Anzahl Optuna-Versuche
+    args = parser.parse_args() # Liest Parameter aus CLI
 
     # Create & run study, maximizing validation F1
-    study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=args.n_trials, show_progress_bar=True)
+    study = optuna.create_study(direction="maximize") # Ziel: Maximiere den F1-Score
+    study.optimize(objective, n_trials=args.n_trials, show_progress_bar=True) # Führt mehrere Trials durch
+    # Für jeden Trial neue Hyperparameter, komplettes Training
 
     # Print out best trial
+    # Bestes F1 und Hyperparameter ausgeben
     print(f'=================================================================\nBest trial (val_f1): {study.best_value:.4f}')
     print(f'  Params:')
     print(study.best_params.items())
@@ -89,3 +106,12 @@ if __name__ == "__main__":
     # info on training time
     time_passed = time.time()-start_timestamp
     print(f'\nTraining finished in {time_passed//3600}h {(time_passed%3600)//60}min {time_passed%60:.0f}s\n')
+
+    """
+    Ideen:
+    - Testset nicht für jeden Trial benutzen --> Testset nur nach Optuna evaluieren
+    - Freeze Backbone Booleans statt ints setzen (Form: True, False statt 0,1)
+    - Nicht nur besten Score, sondern auch bestes Modell speichern
+    - Learning Rate Scheduler hinzufügen
+    - Backbone teilweise unfreezen
+    """
--- src/training.py
+++ src/training.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from src.evaluation import evaluate
 
 
-class YawDDclassifier(nn.Module):
+class YawDDclassifier(nn.Module): #Klasse für Neuronales Netz
     def __init__(self, dropout):
         super().__init__()
 
@@ -17,6 +17,7 @@ class YawDDclassifier(nn.Module):
         self.feature_extractor = nn.Sequential(*list(backbone.children())[:-1]) # keep only the model backbone and remove the final head
         
         # temporal attention pooling
+        # Fokus auf die Frames legen, in denen tatsächlich gegähnt wird. Frames, in denen nichts relevantes passiert, werden weniger beachtet.
         self.attn = nn.Sequential(
             nn.Linear(backbone.fc.in_features, 128),
             nn.Tanh(),
@@ -46,7 +47,8 @@ class YawDDclassifier(nn.Module):
 
         # attention pooling over time
         scores = self.attn(x)               # (B, T, 1)
-        weights = torch.softmax(scores, dim=1)
+        weights = torch.softmax(scores, dim=1) #Rechnet Scores in Wahrscheinlichkeit um, Summe über Zeit =1
+        # Wichtige Frames bekommen hohe Gewichte, unwichtige quasi nahe 0
         pooled = (x * weights).sum(dim=1)   # (B, F)
 
         # final logits
@@ -54,6 +56,7 @@ class YawDDclassifier(nn.Module):
         return logits
     
 
+#Traniert das Modell
 def trainer(trainloader,
             valloader,
             model,
@@ -62,55 +65,100 @@ def trainer(trainloader,
             freeze_backbone, 
             device):
     
+    #speichert bestes Ergebnis
     best_f1 = 0
     best_epoch = 0
 
     # objective function is binary cross entropy loss with logits 
-    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(2.0))
+    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(2.0)) #positive Klasse wird stärker gewichtet (bei Klassenungleichgewicht)
     
     # set non-trainable parameters
+    # ResNet wird nicht trainiert
+    # Backbone wird eingefroren, evt, zu restiktiv, könnte überarbeitet werden.
     if freeze_backbone:
         for p in model.feature_extractor.parameters():
-            p.requires_grad=False
+            p.requires_grad=False #keine Gradienten, keine Updates
+            # Modell lernt nur: Attention und Klassifikations-Head
+    
+    #Idee, um layer4 zu trainieren: (oder je nach Anzahl Epochen dynamisch gestalten)
+    """
+     WIRD IN MAIN ZUFÄLLIG ÜBER OPTUNA GELÖST
+    if freeze_backbone:
+    for p in model.feature_extractor.parameters():
+        p.requires_grad = False
+
+    # letzte Schicht wieder freigeben
+    for p in model.feature_extractor[-1].parameters():
+        p.requires_grad = True
+    """
 
     # Get trainable parameters and hand to optimizer
+    # Nur trainierbare Parameter auswählen
     tp = [p for p in model.parameters() if p.requires_grad]
+    #L2-Regularisierung: Fügt der Loss-Funktion eine Strafe proportional zum Quadrat der Gewichte des Modells hinzu
+    # --> kleinere, besser verteilte Gewichte, reduziert Overfitting
     optimizer = optim.AdamW(tp, lr=lr, weight_decay=1e-2) # AdamW uses weight decay with default 1e-2
+    """
+    WIRD IN MAIN ZUFÄLLIG ÜBER OPTUNA GELÖST
+    Lernrate könnte angepasst werden:
+    lr = lr*0.1 --> Modell lernt langsamer
+    """
     
     # summary(model)
 
     # train loop
+    # Wiederholt Training für mehrere Epochen
     for epoch in range(epochs): 
 
         # init running loss
+        # Loss: Differenz zwischen den Vorhersagewerten und den wahren Werten (Labels)
+        # --> Verschiedene Loss-Funktionen möglich
+        # Running Loss: running loss is the cumulative average loss over a certain number of batches during the training proces
+        # --> Stabilere und flüssigere Schätzung der Modell-Performance
         running_loss = 0
 
         # go through all data
-        model.train()
-        for frames, labels in tqdm(trainloader, desc=f'Epoch {epoch}'):
+        model.train() #setzt Modell in den Trainingsmodus
+        for frames, labels in tqdm(trainloader, desc=f'Epoch {epoch}'): #iteriert über Trainingsdaten
             frames, labels = frames.to(device), labels.to(device) # shift data to device
 
             # forward + backward pass
+            # Vorwärtsdurchlauf, Loss berechnen, Gradienten berechnen
             optimizer.zero_grad()
             logits = model(frames)          
             loss    = criterion(logits, labels)
-            loss.backward()        
+            loss.backward() 
+            # Verhindert exploding Gradient Problem: Gradient könnte unendlich groß werden
+            # Exploding gradients occur when gradients grow too large during backpropagation, 
+            # leading to unstable weight updates and divergence in loss. 
+            # When derivatives or weights are greater than 1, their repeated multiplication 
+            # across layers leads to exponential growth       
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0) # gradient clipping                
-            optimizer.step()
+            optimizer.step() #Gewichte updaten
 
             # update running loss
-            running_loss += loss.item()
+            running_loss += loss.item() #Loss aufsummieren
         
-        print(f'  Loss: {running_loss:0.4f}')
+        print(f'  Loss: {running_loss:0.4f}') #Ausgabe
 
         # evaluate train and validation data
+        # Berechne Performance-Metrics
         train_metrics = evaluate(trainloader, model, device)
         val_metrics = evaluate(valloader, model, device)
 
+        # Accuracy: Verhältnis zwischen richtigen und falschen Vorhersagen
+        # F1:F-score or F-measure is a measure of predictive performance. 
+        # It is calculated from the precision and recall of the test, where the precision is the number of true positive 
+        # results divided by the number of all samples predicted to be positive, 
+        # including those not identified correctly, and the recall is the number of true positive results 
+        # divided by the number of all samples that should have been identified as positive.
+        # F1c: ???
         print(f"Train Acc: {train_metrics['accuracy']:.3f}   --   Val Acc: {val_metrics['accuracy']:.3f}")
         print(f"Train F1: {train_metrics['f1']:.3f}   --   Val F1c: {val_metrics['f1']:.3f}")  
 
         # Save best model checkpoint
+        # Speichert bestes Ergebnis und gibt dieses aus
+        # Speichert NICHT das beste Modell --> optimierbar?
         if val_metrics['f1'] > best_f1:
             best_f1 = val_metrics['f1']
             best_epoch = epoch
