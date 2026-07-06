9a772394a3767adf9c63cb58a99cca3e954f0bff
Er2105 <erik.dittmann@studmail.w-hs.de>
2026-07-06 17:52:01 +0200

    Langes Training über Nacht

--- main.py
+++ main.py
@@ -182,9 +182,9 @@ if __name__ == "__main__":
     # get args 
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, default='YawDD')
-    parser.add_argument("--num_frames", type=int, default=2) # Anzahl Frames pro Sample
-    parser.add_argument("--epochs", type=int, default=2) # Anzahl Trainingsdurchläufe
-    parser.add_argument("--n_trials", type=int, default=1) # Anzahl Optuna-Versuche
+    parser.add_argument("--num_frames", type=int, default=32) # Anzahl Frames pro Sample
+    parser.add_argument("--epochs", type=int, default=25) # Anzahl Trainingsdurchläufe
+    parser.add_argument("--n_trials", type=int, default=5) # Anzahl Optuna-Versuche
     args = parser.parse_args() # Liest Parameter aus CLI
 
     # Create & run study, maximizing validation F1
